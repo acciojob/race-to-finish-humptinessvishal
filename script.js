eab8000c2471ec317f62cc1ef421c0700b14bba2
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-08-28 21:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function getRandomTime(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function createRandomPromise() {
+	const randomTime = getRandomTime(1000, 5000);
+    return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(`Promise resolved after ${randomTime}ms`);
+		}, randomTime);
+	});
+}
+
+    // Add your promises to the existing promises array
+for (let i = 0; i < 5; i++) {
+	promises.push(createRandomPromise());
+}
+Promise.any(promises)
+	.then((result) => {
+		const div = document.getElementById("output");
+		div.textContent = result;
+	})
+	.catch((error) => {
+		console.error("All promises rejected:", error);
+	});
